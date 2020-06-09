1d6ea9459d3e3f168987c6edda2613b2f09a6c0b
blake <bleak@bleak.local>
2020-06-08 23:12:52 -0600

    Remove color from links

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,13 @@ export default function Home() {
         <img style={{width: "70vw"}} src="/subloser_ii.jpg"/>
       </Section>
       <Section direction="column">
-        <ListenButton color="#57DA10" link="https://open.spotify.com/artist/2eGR1MENSdFUpHZ4x5wIri?autoplay=true&v=A">
+        <ListenButton link="https://open.spotify.com/artist/2eGR1MENSdFUpHZ4x5wIri?autoplay=true&v=A">
           spotify
         </ListenButton>
-        <ListenButton color="fuchsia" link="https://itunes.apple.com/us/artist/subloser/1352524116?ign-gact=3&ls=1">
+        <ListenButton link="https://itunes.apple.com/us/artist/subloser/1352524116?ign-gact=3&ls=1">
           apple music
         </ListenButton>
-        <ListenButton>
+        <ListenButton link="https://www.youtube.com/playlist?playnext=1&list=PLAtFjrN5j3tYa6NP-amOGwKBV1-RplSrt&feature=gws_kp_artist">
           youtube
         </ListenButton>
       </Section>
